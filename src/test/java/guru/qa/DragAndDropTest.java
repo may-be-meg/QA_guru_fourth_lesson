93ab5c02d92671512de30cc1d98cbab0ed5c37da
Taisia <meg826733@gmail.com>
2022-09-03 22:46:41 +0500

    fix

--- src/test/java/guru/qa/DragAndDropTest.java
+++ src/test/java/guru/qa/DragAndDropTest.java
@@ -1,5 +1,18 @@
 package guru.qa;
 
-public class DragAndDropTest {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class DragAndDropTest {
+    @Test
+    void DragAndDrop() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //actions().clickAndHold($("#column-a")).moveByOffset(850, 150).release().perform();
+        $("#column-a").dragAndDropTo($("#column-b"));
+        $("#column-b").shouldHave(text("A"));
+    }
 }
--- src/test/java/guru/qa/SelectPricingTest.java
+++ src/test/java/guru/qa/SelectPricingTest.java
@@ -7,14 +7,15 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelectPricingTest {
-
     @Test
     void shouldFindComparePlans() {
 
         open("https://github.com/");
+        //в хэдэре находим элемент Pricing и наводим на него мышкой
         $(".HeaderMenu").$(byText("Pricing")).hover();
+        //в появившемся меню выбираем пункт Compare plans
         $(byText("Compare plans")).click();
+        //проверяем, что открылась нужная страница (находим Compare features)
         $(".h1").shouldHave(text("Compare features"));
-        sleep(5000);
     }
 }
